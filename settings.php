9109c2fef70d0e5cadf360f6b9741e961b0890b9
Amit Sonkhiya <amit@astech.club>
2018-09-21 22:36:41 +0530

    Metadata and WP coding styles v1.3

--- settings.php
+++ settings.php
@@ -1,32 +1,35 @@
 <?php
 /*
 Plugin Name: Categories in Hierarchical Order
-Plugin URI: http://dev.fellowtuts.com/categories-in-hierarchical-order-plugin/
+Plugin URI: https://fellowtuts.com/categories-in-hierarchical-order-plugin/
 Description: This plugin maintains the hierarchical order of categories list in Category tab under your WordPress admin post editor.
-Version: 1.0
-Author: Amit Sonkhiya, Kamal Agrawal
-Author URI: http://dev.fellowtuts.com
+Version: 1.3
+Author: AS Tech Solutions
+Author URI: https://www.astech.club/
+Text Domain: categories-in-hierarchical-order
+Domain Path: /languages
 License: GPLv2 or later
 */
 
-if ( ! class_exists( 'ftChangeTaxonomyCheckboxlistOrder' ) ){
-	
-	class ftChangeTaxonomyCheckboxlistOrder {	
-		
-		function ftChangeTaxonomyCheckboxlistOrder(){
-	
-			function changeTaxonomyCheckboxlistOrder( $args, $post_id)
-			{
-				if ( isset( $args['taxonomy']))
-		 			$args['checked_ontop'] = false;
-    	   		return $args;
-			}
-	
-		add_filter('wp_terms_checklist_args','changeTaxonomyCheckboxlistOrder',10,2);
-	}
-	
-} // class ends here
+if ( ! class_exists( 'ftChangeTaxonomyCheckboxlistOrder' ) ) {
 
-$fttaxonomychangeorder = new ftChangeTaxonomyCheckboxlistOrder();
+	class ftChangeTaxonomyCheckboxlistOrder {
+
+		function __construct() {
+
+			function changeTaxonomyCheckboxlistOrder( $args, $post_id ) {
+				if ( isset( $args['taxonomy'] ) ) {
+					$args['checked_ontop'] = false;
+				}
+
+				return $args;
+			}
+
+			add_filter( 'wp_terms_checklist_args', 'changeTaxonomyCheckboxlistOrder', 10, 2 );
+		}
+
+	} // class ends here
+
+	$fttaxonomychangeorder = new ftChangeTaxonomyCheckboxlistOrder();
 
 }// top most if condition ends here
