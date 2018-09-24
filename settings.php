ac21dabe869da39804f1f794aaf87d06d6a2cfde
Amit Sonkhiya <amit@astech.club>
2018-09-24 15:36:14 +0530

    Direct file access blocked

--- settings.php
+++ settings.php
@@ -1,9 +1,12 @@
-<?php
+<?php if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
 /*
 Plugin Name: Categories in Hierarchical Order
 Plugin URI: https://www.astech.club/wordpress-javascript-jquery-plugins/categories-hierarchical-order/
 Description: This plugin maintains the hierarchical order of categories list in Category tab under your WordPress admin post editor.
-Version: 1.3
+Version: 1.3.1
 Author: AS Tech Solutions
 Author URI: https://www.astech.club/
 Text Domain: categories-in-hierarchical-order
